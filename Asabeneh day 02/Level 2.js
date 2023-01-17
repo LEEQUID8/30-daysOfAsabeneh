67281816c73e08859c5e01a2b2bc518d1358565e
LEEQUID8 <112183856+LEEQUID8@users.noreply.github.com>
2023-01-17 21:11:16 +0100

    Add files via upload
    made some changes

---
+++ Asabeneh day 02/Level 2.js
@@ -0,0 +1,42 @@
+//Question 1: Using console.log() print out the following statement
+var quote1 = 'There is no exercise better for the heart than reaching down and lifting people up'
+//Question 2: Using console.log() print out the following quote by Mother Teresa
+let quote2 = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+// checking datatypes using typeof
+//Question 3:Check if typeof '10' is exactly equal to 10. If not make it exactly equal 
+console.log(typeof "10" === 10); 
+//Question 4: Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+let parsefloat = '9.8'
+console.log(parseFloat==age)
+console.log(typeof '10')
+//Question 5; Check if 'on' is found in both python and jargon
+let me = python
+console.log(me.match(on))
+let him = jargon
+console.log(him.match(on))
+//Question 6: I hope this course is not full of jargon. Check if jargon is in the sentence.
+let jb="I hope this course is not full of jargon"
+console.log(jb.match("jargon"))
+//generatting random number
+//Question 7: Generate a random number between 0 and 100 inclusively.
+let randomNum = Math.random()
+let numBtnZeroAndHundred = randomNum * 111
+console.log(numBtnZeroAndHundred)
+//Question 8: Generate a random number between 50 and 100 inclusively.
+let numBtnFiftyAndHundred = randomNum * 100
+console.log (numBtnFiftyAndHundred)
+//Question 9: Generate a random number between 0 and 255 inclusively.
+let numBtnZeroAndTwoAndFiftyFive = randomNum * 255
+console.log(numBtnZeroAndTwoAndFiftyFive)
+//Question 10: Access the 'JavaScript' string characters using a random number.
+const char = 'JavaScript';
+console.log(char[Math.floor(Math.random() * 3)]);
+//Question 11: Use console.log() and escape characters to print the following pattern.
+console.log ('1/1/1/1/1')
+console.log ('2/1/2/4/8')
+console.log ('3/1/3/9/27')
+console.log ('4/1/4/16/27')
+console.log ('5/1/5/25/125')
+//Question 12: Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let final = 'You cannot end a sentence with because because because is a conjunction'
+console.log(final.substr(' because because because '))
---
+++ Asabeneh day 02/readme.md
@@ -0,0 +1,20 @@
+-Primitive Data Types
+-Non-Primitive Data Types
+Numbers
+-Declaring Number Data Types
+-Math Object
+-Random Number Generator
+-Strings
+-String Concatenation
+-Data Types
+-Concatenating Using Addition Operator
+-Long Literal Strings
+-Escape Sequences in Strings
+-Template Literals (Template Strings)
+-String Methods
+-Checking Data Types and Casting
+-Checking Data Types
+-Changing Data Type (Casting)
+-String to Int
+-String to Float
+-Float to Int
